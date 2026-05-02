52f47de36f2c33bb73ef2d4042d8dc53faa10708
Juan Camilo García <juan.garciagut@upb.edu.co>
2026-05-02 11:53:25 -0500

    Remove pip install streamlit line

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ Original file is located at
     https://colab.research.google.com/drive/19QxeBZpFiuQrOfajem4EsOpJXvoGqu8V
 """
 
-pip install streamlit
+
 
 import streamlit as st
 import psycopg2 # El conector para Postgres
